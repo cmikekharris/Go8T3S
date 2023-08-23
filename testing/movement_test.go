f6045fa63ebde5a68b5a6c4bb213f2a2da4d1689
Michael Harris <mike@tublet.net>
2023-08-23 09:15:03 +0100

    Add placeholder for movement tests

---
+++ testing/movement_test.go
@@ -0,0 +1,19 @@
+package testing
+
+import "testing"
+
+func TestMoveLeft(t *testing.T) {
+
+}
+
+func TestMoveRight(t *testing.T) {
+
+}
+
+func TestMoveUp(t *testing.T) {
+
+}
+
+func TestMoveDown(t *testing.T) {
+
+}
