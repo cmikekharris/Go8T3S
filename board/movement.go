711c5abd0b86543c572aa8a8d4e47d47ac8da012
Michael Harris <mike@tublet.net>
2023-08-23 09:13:46 +0100

    Create placeholder functions for board movement

---
+++ board/movement.go
@@ -0,0 +1,21 @@
+package board
+
+import (
+	"fmt"
+)
+
+func MoveLeft() {
+	fmt.Println("Move left")
+}
+
+func MoveRight() {
+	fmt.Println("Move right")
+}
+
+func MoveUp() {
+	fmt.Println("Move up")
+}
+
+func MoveDown() {
+	fmt.Println("Move down")
+}
