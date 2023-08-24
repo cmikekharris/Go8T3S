62e15e8244716cd287539b3b9e407486defac6d9
Michael Harris <mike@tublet.net>
2023-08-24 16:14:38 +0100

    Add print function for the board

--- tree/tree.go
+++ tree/tree.go
@@ -7,24 +7,31 @@ import (
 )
 
 type Tree struct {
-	parent *Tree
-	uo    *Tree
-	down  *Tree
-	left  *Tree
-	right *Tree
-	board [3][3]int
+	Parent *Tree
+	Up     *Tree
+	Down   *Tree
+	Left   *Tree
+	Right  *Tree
+	Cost   int
+	Board  [3][3]int
 }
 
-func MakeTree() *Tree {
+func MakeTree() Tree {
 	fmt.Println("Generating Tree...")
 
 	t := Tree{}
 
-	t.board = board.MakeBoard()
+	t.Board = board.MakeBoard()
 
-	return &t
+	return t
 }
 
-func PrintTree() {
-
+func PrintTree(t Tree) {
+	fmt.Printf("\t\t-------------\n")
+	fmt.Printf("\t\t| %d | %d | %d |\n", t.Board[0][0], t.Board[0][1], t.Board[0][2])
+	fmt.Printf("\t\t-------------\n")
+	fmt.Printf("\t\t| %d | %d | %d |\n", t.Board[1][0], t.Board[1][1], t.Board[1][2])
+	fmt.Printf("\t\t-------------\n")
+	fmt.Printf("\t\t| %d | %d | %d |\n", t.Board[2][0], t.Board[2][1], t.Board[2][2])
+	fmt.Printf("\t\t-------------\n")
 }
