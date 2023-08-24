96a940a35eb712410f2b8f42a4f60179f576801c
Michael Harris <mike@tublet.net>
2023-08-24 16:15:11 +0100

    Refactor to one function

--- board/movement.go
+++ board/movement.go
@@ -4,18 +4,50 @@ import (
 	"fmt"
 )
 
-func MoveLeft() {
-	fmt.Println("Move left")
+func FindEmptySpace(board [3][3]int) (int, int) {
+	fmt.Println("Finding empty space...")
+
+	fmt.Println(board)
+
+	for row, column := range board {
+		for cell := range column {
+			if board[row][cell] == 0 {
+				return row, cell
+			}
+		}
+	}
+
+	return -1, -1
 }
 
-func MoveRight() {
-	fmt.Println("Move right")
-}
+func Move(board [3][3]int, direction string) [3][3]int {
 
-func MoveUp() {
-	fmt.Println("Move up")
-}
+	fmt.Println("Move", direction, "...")
 
-func MoveDown() {
-	fmt.Println("Move down")
+	newBoard := board
+	oldRow, oldCell := FindEmptySpace(board)
+	newRow, newCell := oldRow, oldCell
+
+	switch direction {
+	case "up":
+		newRow = oldRow - 1
+	case "down":
+		newRow = oldRow + 1
+	case "left":
+		newCell = oldCell - 1
+	case "right":
+		newCell = oldCell + 1
+	}
+
+	// Return existing board if out of bounds
+	if newRow < 0 || newRow > 2 || newCell < 0 || newCell > 2 {
+		return board
+	}
+
+	// Swap empty space with number
+	oldValue := board[newRow][newCell]
+	newBoard[newRow][newCell] = 0
+	newBoard[oldRow][oldCell] = oldValue
+
+	return newBoard
 }
