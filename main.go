ddfce57cdb84b82187cc571640ed410900078315
Michael Harris <mike@tublet.net>
2023-08-31 16:33:26 +0100

    Added menu and welcome / instruction messages

--- main.go
+++ main.go
@@ -8,22 +8,77 @@ import (
 )
 
 func main() {
-	fmt.Println("Eight Tile State Space Searching Game written in Go - Go8T3S")
+	fmt.Println(banner())
+	fmt.Println(instructions())
 
-	root := tree.MakeTree()
+	// Make a starting board
+	root := tree.MakeBranch([3][3]int{{-2}})
+	// Current branch
+	cb := &root
 
-	fmt.Println(root)
-	tree.PrintTree(root)
+	tree.PrintBoard(cb.Board)
 
-	root.Board = board.Move(root.Board, "up")
-	tree.PrintTree(root)
+	b := [3][3]int{{-1}}
+	var choice string
 
-	root.Board = board.Move(root.Board, "down")
-	tree.PrintTree(root)
+	for {
+		fmt.Print("Enter your choice: ")
+		fmt.Scanf("%s", &choice)
 
-	root.Board = board.Move(root.Board, "left")
-	tree.PrintTree(root)
+		switch choice {
+		case "x":
+			fmt.Println("Exiting...")
+			return
+		case "a":
+			b = board.Move(cb.Board, "left")
+		case "d":
+			b = board.Move(cb.Board, "right")
+		case "w":
+			b = board.Move(cb.Board, "up")
+		case "s":
+			b = board.Move(cb.Board, "down")
+		case "i":
+			fmt.Println(instructions())
+		case "p":
+			tree.PrintBoard(cb.Board)
+		}
 
-	root.Board = board.Move(root.Board, "right")
-	tree.PrintTree(root)
+		// Store the new board in the tree and advance current pointer
+		nb := tree.MakeBranch(b)
+		cb.Moves = append(cb.Moves, &nb)
+		cb = &nb
+
+		tree.PrintBoard(cb.Board)
+
+		if tree.CheckWinningState(*cb) {
+			return
+		}
+	}
+}
+
+func banner() string {
+	return `
+	$$$$$$$$\ $$\           $$\        $$\           $$$$$$$$\ $$\ $$\                  $$$$$$\                                    
+	$$  _____|\__|          $$ |       $$ |          \__$$  __|\__|$$ |                $$  __$$\                                   
+	$$ |      $$\  $$$$$$\  $$$$$$$\ $$$$$$\            $$ |   $$\ $$ | $$$$$$\        $$ /  \__| $$$$$$\  $$$$$$\$$$$\   $$$$$$\  
+	$$$$$\    $$ |$$  __$$\ $$  __$$\\_$$  _|           $$ |   $$ |$$ |$$  __$$\       $$ |$$$$\  \____$$\ $$  _$$  _$$\ $$  __$$\ 
+	$$  __|   $$ |$$ /  $$ |$$ |  $$ | $$ |             $$ |   $$ |$$ |$$$$$$$$ |      $$ |\_$$ | $$$$$$$ |$$ / $$ / $$ |$$$$$$$$ |
+	$$ |      $$ |$$ |  $$ |$$ |  $$ | $$ |$$\          $$ |   $$ |$$ |$$   ____|      $$ |  $$ |$$  __$$ |$$ | $$ | $$ |$$   ____|
+	$$$$$$$$\ $$ |\$$$$$$$ |$$ |  $$ | \$$$$  |         $$ |   $$ |$$ |\$$$$$$$\       \$$$$$$  |\$$$$$$$ |$$ | $$ | $$ |\$$$$$$$\ 
+	\________|\__| \____$$ |\__|  \__|  \____/          \__|   \__|\__| \_______|       \______/  \_______|\__| \__| \__| \_______|
+                      $$\   $$ |                                                                                                       
+                      \$$$$$$  |                                                                                                       
+                       \______/	                                             
+	`
+}
+
+func instructions() string {
+	return `
+	w - Up
+	s - Down
+	a - Left
+	d - Right
+	i - Print instructions
+	x - Exit game
+	`
 }
