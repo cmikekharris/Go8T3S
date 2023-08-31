9f12cedc97594a25f71447ddb28704ae05280506
Michael Harris <mike@tublet.net>
2023-08-31 16:33:02 +0100

    Check for winning state and allow for test board

--- tree/tree.go
+++ tree/tree.go
@@ -8,30 +8,69 @@ import (
 
 type Tree struct {
 	Parent *Tree
-	Up     *Tree
-	Down   *Tree
-	Left   *Tree
-	Right  *Tree
+	Moves  []*Tree
 	Cost   int
 	Board  [3][3]int
 }
 
-func MakeTree() Tree {
-	fmt.Println("Generating Tree...")
+func MakeBranch(b [3][3]int) Tree {
+	fmt.Println("Generating branch...")
 
-	t := Tree{}
+	var t = Tree{}
+	t.Board = b
 
-	t.Board = board.MakeBoard()
+	if t.Board[0][0] == -1 {
+		t.Board = board.MakeBoard()
+	}
+
+	if t.Board[0][0] == -2 {
+		t.Board = board.MakeTestBoard()
+	}
 
 	return t
 }
 
 func PrintTree(t Tree) {
-	fmt.Printf("\t\t-------------\n")
-	fmt.Printf("\t\t| %d | %d | %d |\n", t.Board[0][0], t.Board[0][1], t.Board[0][2])
-	fmt.Printf("\t\t-------------\n")
-	fmt.Printf("\t\t| %d | %d | %d |\n", t.Board[1][0], t.Board[1][1], t.Board[1][2])
-	fmt.Printf("\t\t-------------\n")
-	fmt.Printf("\t\t| %d | %d | %d |\n", t.Board[2][0], t.Board[2][1], t.Board[2][2])
-	fmt.Printf("\t\t-------------\n")
+	// fmt.Println("Printing tree...")
+	// fmt.Println("Number of branches : ", len(t.Moves))
+	// fmt.Println("Parent : ", t.Parent)
+
+	// for run := true; run; {
+	// 	fmt.Println("Cost : ", t.Cost)
+	// 	PrintBoard(t.Board)
+
+	// 	for _, branch := range t.Moves {
+	// 		PrintTree(branch)
+	// 	}
+	// 	run = false
+	// }
+}
+
+func PrintBranch(t Tree) {
+	fmt.Println("Printing branch...")
+	fmt.Println("Parent : ", t.Parent)
+	fmt.Println("Moves : ", t.Moves)
+	fmt.Println("Cost : ", t.Cost)
+	PrintBoard(t.Board)
+}
+
+func PrintBoard(b [3][3]int) {
+	fmt.Printf("\t\tCurrent board\t\t\t\t\tWinning Board\n")
+	fmt.Printf("\t\t-------------\t\t\t\t\t-------------\n")
+	fmt.Printf("\t\t| %d | %d | %d |\t\t\t\t\t| 1 | 2 | 3 |\n", b[0][0], b[0][1], b[0][2])
+	fmt.Printf("\t\t-------------\t\t\t\t\t-------------\n")
+	fmt.Printf("\t\t| %d | %d | %d |\t\t\t\t\t| 8 | 0 | 4 |\n", b[1][0], b[1][1], b[1][2])
+	fmt.Printf("\t\t-------------\t\t\t\t\t-------------\n")
+	fmt.Printf("\t\t| %d | %d | %d |\t\t\t\t\t| 7 | 6 | 5 |\n", b[2][0], b[2][1], b[2][2])
+	fmt.Printf("\t\t-------------\t\t\t\t\t-------------\n")
+}
+
+func CheckWinningState(t Tree) bool {
+	// Check for winning game state
+	if t.Board == board.GoalState() {
+		fmt.Println("\t\t\t\t== WINNER WINNER WINNER ==")
+		return true
+	}
+
+	return false
 }
