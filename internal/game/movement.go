1ac4657acaa8a1f97c0f570ec4b7e71e2def25c5
Michael Harris <mike@tublet.net>
2023-09-20 20:25:48 +0100

    Collect movement functions together

---
+++ internal/game/movement.go
@@ -0,0 +1,50 @@
+package game
+
+import (
+	"fmt"
+)
+
+func (board GameBoard) Move(direction string) [3][3]int {
+	fmt.Println("Move", direction, "...")
+
+	newBoard := board
+	oldRow, oldCell := findEmptySpace(board)
+	newRow, newCell := oldRow, oldCell
+
+	switch direction {
+	case "up":
+		newRow = oldRow - 1
+	case "down":
+		newRow = oldRow + 1
+	case "left":
+		newCell = oldCell - 1
+	case "right":
+		newCell = oldCell + 1
+	}
+
+	// Return existing board if out of bounds
+	if newRow < 0 || newRow > 2 || newCell < 0 || newCell > 2 {
+		return board
+	}
+
+	// Swap empty space with number
+	oldValue := board[newRow][newCell]
+	newBoard[newRow][newCell] = 0
+	newBoard[oldRow][oldCell] = oldValue
+
+	return newBoard
+}
+
+func findEmptySpace(board [3][3]int) (int, int) {
+	fmt.Println("Finding empty space...")
+
+	for row, column := range board {
+		for cell := range column {
+			if board[row][cell] == 0 {
+				return row, cell
+			}
+		}
+	}
+
+	return -1, -1
+}
