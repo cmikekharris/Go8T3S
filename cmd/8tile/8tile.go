ebc38a2d50aceaea2a508e230a263cdfc18a0210
Michael Harris <mike@tublet.net>
2023-09-20 20:24:50 +0100

    Move menu back to main to de-clutter

--- cmd/8tile/8tile.go
+++ cmd/8tile/8tile.go
@@ -3,13 +3,57 @@ package main
 import (
 	"fmt"
 
-	"github.com/cmikekharris/Go8T3S/internal/board"
+	"github.com/cmikekharris/Go8T3S/internal/game"
 	"github.com/cmikekharris/Go8T3S/internal/manual"
+	"github.com/cmikekharris/Go8T3S/internal/output"
+	"github.com/cmikekharris/Go8T3S/internal/tree"
 )
 
 func main() {
 	fmt.Println(manual.Banner())
 	fmt.Println(manual.Instructions())
-	
-	board.Run()
+
+	var choice string
+	var root = tree.Branch{}
+	cb := root.Grow(game.MakeBoard())
+
+	// Start game running loop
+	for {
+		fmt.Print("Enter your choice: ")
+		fmt.Scanf("%s", &choice)
+
+		switch choice {
+		case "x":
+			fmt.Println("Exiting...")
+			return
+		case "a":
+			b := cb.Board.Move("left")
+			cb = cb.Grow(b)
+		case "d":
+			b := cb.Board.Move("right")
+			cb = cb.Grow(b)
+		case "w":
+			b := cb.Board.Move("up")
+			cb = cb.Grow(b)
+		case "s":
+			b := cb.Board.Move("down")
+			cb = cb.Grow(b)
+		case "i":
+			fmt.Println(manual.Instructions())
+		case "p":
+			output.DisplayBoard(cb.Board)
+		case "b":
+			output.DisplayBranch(*cb)
+		case "t":
+			output.DisplayTree(root)
+		}
+
+		if game.WinningState(cb.Board) {
+			fmt.Println("\t\t\t\t== WINNER WINNER WINNER ==")
+			fmt.Println("Winning solution")
+			fmt.Println("----------------")
+			output.DisplayTree(root)
+			return
+		}
+	}
 }
